53b1377cfa461e517f430d83ac113b82ba922c64
Ruslana Pavliuk <pavliuk_r_v@protonmail.com>
2022-07-13 17:13:25 +0300

    second part is ready also. only one bug left

--- hw21.js
+++ hw21.js
@@ -16,24 +16,37 @@ class FullPost {
 
     renderPostBody() {
         let newPost = '';
-        newPost = `<h1>${this.title}</h1>
-                   <p>${this.description}</p>`
+        newPost = `<h1 id="post_title">${this.title}</h1>
+                   <p id="post_body">${this.description}</p>`
         this.element.innerHTML = newPost;
     }
 
     renderComments(data) {
         let array = data;
+        let div = document.createElement('div');
+        div.setAttribute('id', 'comments')
+
         for (let el of array) {
             let p = document.createElement('p');
             let text = document.createTextNode(`Comment: ${el.name}`);
             p.appendChild(text)
-            this.element.appendChild(p);
+            div.appendChild(p);
         }
+        this.element.appendChild(div);
     }
 
-    addButton() {
+    addComments() {
+        let newComment = document.createElement("p")
+        newComment.setAttribute('id', 'new_comment');
+        let input = document.createElement('input');
+        input.setAttribute('type','text');
+        input.setAttribute('value', 'text your comment here')
+        newComment.appendChild(input);
+
         let btn = document.createElement('button');
         btn.innerHTML = 'Add Comment';
+
+        this.element.appendChild(newComment);
         this.element.appendChild(btn);
     }
 
@@ -49,5 +62,28 @@ getPost('https://jsonplaceholder.typicode.com/posts/1')
     .then (getPost('https://jsonplaceholder.typicode.com/posts/1/comments')
     .then(data => {
         post1.renderComments(data);
-        post1.addButton()
+        post1.addComments()
         }))
+
+
+//Second part of homework
+
+console.log(1);
+
+setTimeout(function () {
+    console.log(2);
+}, 100);
+
+setTimeout(function () {
+    console.log(3);
+}, 0);
+
+new Promise(function (resolve) {
+    setTimeout( () => resolve(), 0 )
+
+}).then(() => {
+    console.log(4);
+});
+
+console.log(5);
+
